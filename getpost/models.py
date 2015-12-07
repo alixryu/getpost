7576b46a2d37e5b695ba18bf6d2ea22e51eac614
heavenk21 <heavenk21@gmail.com>
2015-12-06 20:38:54 -0500

    Add(models.py): create table objects for getpost
    fixes #1

---
+++ getpost/models.py
@@ -0,0 +1,56 @@
+""":mod:`getpost.models` --- model module of getpost
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+from sqlalchemy import Column, DateTime, Enum, ForeignKey, Integer, String
+
+from orm import Base
+
+
+class Package(Base):
+    __tablename__ = 'package'
+
+    id = Column(Integer, primary_key=True)
+    student_id = Column(String)
+    arrival_date = Column(DateTime)
+    pickup_date = Column(DateTime)
+    received_by = Column(String)
+    status = Column(
+        Enum('picked_up', 'not_picked_up', name='package_status_type')
+    )
+
+
+class Notification(Base):
+    __tablename__ = 'notification'
+
+    id = Column(Integer, primary_key=True)
+    package_id = Column(Integer, ForeignKey('package.id'))
+    email_address = Column(String)
+    send_date = Column(DateTime)
+    send_count = Column(DateTime)
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    email_address = Column(String)
+    password = Column(String)
+    role = Column(Enum('student', 'employee', name='user_type'))
+
+
+class Employee(Base):
+    __tablename__ = 'employee'
+
+    id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+
+
+class Student(Base):
+    __tablename__ = 'student'
+
+    id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    ocmr = Column(Integer)
+    t_number = Column(Integer)
