c266e9bdc2eb42ee048af659c0b20d0acdefc1a0
C0urante <cegerton@oberlin.edu>
2015-12-08 22:44:01 -0500

    Add error handling.

--- getpost/__init__.py
+++ getpost/__init__.py
@@ -16,6 +16,18 @@ def create_app(config_obj):
     app.register_blueprint(parcels_blueprint)
     app.register_blueprint(wizards_blueprint)
 
+    @app.errorhandler(500)
+    def internal(error):
+        title = 'Error 500'
+        desc = 'Uh oh! Something went wrong.'
+        return render_template('voldemort.html', title=title, description=desc), 500
+
+    @app.errorhandler(404)
+    def not_found(error):
+        title = 'Error 404'
+        desc = 'This page does not exist.'
+        return render_template('voldemort.html', title=title, description=desc), 500
+
     return app
 
 
