de9c1aa8a837febe1ecb512889a946e58f922a13
C0urante <cegerton@oberlin.edu>
2015-12-15 16:31:33 -0500

    Change links

--- getpost/desk/wizards.py
+++ getpost/desk/wizards.py
@@ -15,7 +15,6 @@ wizards_blueprint = Blueprint(
 
 
 @wizards_blueprint.route('/')
-@wizards_blueprint.route('/view/')
 def wizards_index():
     neg_check = lambda x: x if x >= 1 else 1
     page = neg_check(request.args.get('page', 1, type=int))
@@ -47,13 +46,3 @@ def wizards_index():
     return render_template(
         'wizards.html', students=students, count=page_count
     )
-
-
-@wizards_blueprint.route('/view/all/')
-def wizards_all():
-    return render_template('wizards.html')
-
-
-@wizards_blueprint.route('/view/me/')
-def wizards_user():
-    return render_template('wizards.html')
