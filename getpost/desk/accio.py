24c1d2d6d7295211209653cf6fe1d3e909bafba7
Elizabeth  Bentivegna <elizabeth@ip-120-20.wireless.oberlin.edu>
2015-12-16 21:45:34 -0500

    Simple search function implemented

--- getpost/desk/accio.py
+++ getpost/desk/accio.py
@@ -1,5 +1,5 @@
 from math import ceil
-
+from sqlalchemy.orm.exc import NoResultFound
 from flask import Blueprint, render_template, request, redirect, flash, abort, session as user_session
 
 from . import ACCOUNT_PER_PAGE as page_size
@@ -32,33 +32,52 @@ def accio_index():
     search_params['ocmr'] = request.args.get('ocmr', '', type=str)
     search_params['tnumber'] = request.args.get('tnumber', '', type=str)
 
-    search_params = {key: value for (key, value) in search_params.items() if value != ''}
     db_session = Session()
+    search_all = True
+    search_none = False
 
-    base_query = db_session.query(Student)
+    try:
+        base_query = db_session.query(Student)
+        if (search_params['firstname'] != ''):
+            base_query = base_query.filter(Student.first_name == search_params['firstname'])
+            search_all = False
+        if (search_params['lastname'] != ''):
+            base_query = base_query.filter(Student.last_name == search_params['lastname'])
+            search_all = False
+        if (search_params['preferredname'] != ''):
+            base_query = base_query.filter(Student.alternative_name == search_params['preferredname'])
+            search_all = False
+        if (search_params['ocmr'] != ''):
+            base_query = base_query.filter(Student.ocmr == search_params['ocmr'])
+            search_all = False
+        if (search_params['tnumber'] != ''):
+            base_query = base_query.filter(Student.t_number == search_params['tnumber'])
+            search_all = False
 
-    #for param, val in search_params.items():
+        page_count = int(ceil(base_query.count()/page_size))
 
-        #base_query.filter(Student.param == val)
+        paginated_students = base_query.limit(
+            page_size
+            ).offset(
+            (page-1)*page_size
+            ).from_self().join(Account).all()
 
-    page_count = int(ceil(base_query.count()/page_size))
+        students = []
+        for s_a in paginated_students:
+            student = {}
+            student.update(s_a.as_dict(
+                {'first_name', 'last_name', 'alternative_name', 'ocmr', 't_number'}
+            ))
+            student.update(s_a.account.as_dict(
+                {'email_address', 'role', 'verified'}
+            ))
+            students.append(student)
+        if len(students) == 0:
+            search_none = True
 
-    paginated_students = base_query.limit(
-        page_size
-        ).offset(
-        (page-1)*page_size
-        ).from_self().join(Account).all()
+    except NoResultFound:
+        flash('An error occurred', 'error')
+    finally:
+        db_session.close()
 
-    students = []
-    for s_a in paginated_students:
-        student = {}
-        student.update(s_a.as_dict(
-            {'first_name', 'last_name', 'alternative_name', 'ocmr', 't_number'}
-        ))
-        student.update(s_a.account.as_dict(
-            {'email_address', 'role', 'verified'}
-        ))
-        students.append(student)
-    db_session.close()
-
-    return render_template('accio.html', search_params=search_params, students=students)
+    return render_template('accio.html', search_params=search_params, students=students, search_none=search_none, search_all=search_all)
